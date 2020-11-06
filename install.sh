1af22cd270da68adc6bbd8afdf2107758aa1b7a9
j-wanner <57173138+j-wanner@users.noreply.github.com>
2020-11-06 17:35:57 +0900

    a

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-#sed -i -e '/AuthorizedKeysFile/d' /etc/pacman-mirrors.conf
-
-sed -i -e '/^Method.*/d' /etc/pacman-mirrors.conf
-sed -i -e '/^OnlyCountry.*/d' /etc/pacman-mirrors.conf
-echo -e \\nMethod = rank\\nOnlyCountry = Japan >> /etc/pacman-mirrors.conf
+sed -i -e '/^Method.*/d' /etc/pacman-mirrors.conf;sed -i -e '/^OnlyCountry.*/d' /etc/pacman-mirrors.conf
+echo -e \\nMethod = rank\\nOnlyCountry = Japan >> /etc/pacman-mirrors.conf && sudo pacman-mirrors --fasttrack && sudo pacman -Syu
 
 #for f in .??*
 #do
