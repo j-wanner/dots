34aaf26a8ecf2ea5ac6b1c6f8247c8b2bdedc88b
j-wanner <57173138+j-wanner@users.noreply.github.com>
2020-11-06 16:53:00 +0900

    a

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-for f in .??*
-do
-  [[ "$f" == ".git" ]] && continue
-  [[ "$f" == ".DS_Store" ]] && continue
+#for f in .??*
+#do
+#  [[ "$f" == ".git" ]] && continue
+#  [[ "$f" == ".DS_Store" ]] && continue
+#
+#  echo "$f"
+#done
 
-  echo "$f"
-done
+echo "this is test"
