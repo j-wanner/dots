a667f97608e3c23439d2e989cfb0f800e6be63fe
j-wanner <57173138+j-wanner@users.noreply.github.com>
2020-11-07 02:37:47 +0900

    a

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd ~; git clone https://github.com/j-wanner/dots.git;
+cp -r dots/file/fonts .local/share/
 
 echo -e "export LANG=\"ja_JP.UTF-8\"\nexport XMODIFIERS=\"@im=fcitx\"\nexport XMODIFIER=\"@im=fcitx\"\nexport GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\nexport DefaultIMModule=fcitx" > ~/.xprofile
 echo -e "\nexport GTK_IM_MODULE=fcitx\nexport XMODIFIERS=@im=fcitx\nexport QT_IM_MODULE=fcitx" >> ~/.bashrc
