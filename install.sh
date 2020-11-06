0851f30fe2e9aadd2389a8d657f092e3b92e79fc
j-wanner <57173138+j-wanner@users.noreply.github.com>
2020-11-07 00:45:30 +0900

    a

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ exit
 #=== x
 #==================================
 cd ~ ; git clone https://github.com/j-wanner/dots.git;
-
+cp dots/file/fonts/HackGen /usr/share/fonts/
 echo -e "export LANG=\"ja_JP.UTF-8\"\nexport XMODIFIERS=\"@im=fcitx\"\nexport XMODIFIER=\"@im=fcitx\"\nexport GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\nexport DefaultIMModule=fcitx" > ~/.xprofile
 echo -e "\nexport GTK_IM_MODULE=fcitx\nexport XMODIFIERS=@im=fcitx\nexport QT_IM_MODULE=fcitx" >> ~/.bashrc
 #echo -e "\n\nexec --no-startup-id fcitx" >> ~/.i3/config
