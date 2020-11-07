dbb0691eeac8162b97f971eb74ca5e9412f983ea
j-wanner <57173138+j-wanner@users.noreply.github.com>
2020-11-07 13:12:22 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,20 +3,18 @@
 cd ~; git clone https://github.com/j-wanner/dots.git;
 cp -r dots/file/fonts .local/share/
 
-echo -e "export LANG=\"ja_JP.UTF-8\"\nexport XMODIFIERS=\"@im=fcitx\"\nexport XMODIFIER=\"@im=fcitx\"\nexport GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\nexport DefaultIMModule=fcitx" > ~/.xprofile
-echo -e "\nexport GTK_IM_MODULE=fcitx\nexport XMODIFIERS=@im=fcitx\nexport QT_IM_MODULE=fcitx" >> ~/.bashrc
+#echo -e "export LANG=\"ja_JP.UTF-8\"\nexport XMODIFIERS=\"@im=fcitx\"\nexport XMODIFIER=\"@im=fcitx\"\nexport GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\nexport DefaultIMModule=fcitx" > ~/.xprofile
+#echo -e "\nexport GTK_IM_MODULE=fcitx\nexport XMODIFIERS=@im=fcitx\nexport QT_IM_MODULE=fcitx" >> ~/.bashrc
 #echo -e "\n\nexec --no-startup-id fcitx" >> ~/.i3/config
 
 
+for f in .??*
+do
+  [[ "$f" == ".git" ]] && continue
+  [[ "$f" == ".gitignore" ]] && continue
+  #[[ "$f" == ".DS_Store" ]] && continue
 
-
-
-#for f in .??*
-#do
-#  [[ "$f" == ".git" ]] && continue
-#  [[ "$f" == ".DS_Store" ]] && continue
-#
 #  ln -snfv ~/dotfiles/"$f" ~/
-#  echo "$f"
-#done
+  echo "$f"
+done
 
