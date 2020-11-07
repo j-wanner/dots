867af639b5bee9853a4b97ed38aa0346c8f108f4
j-wanner <57173138+j-wanner@users.noreply.github.com>
2020-11-07 17:15:15 +0900

    a

--- install.sh
+++ install.sh
@@ -14,5 +14,6 @@ do
   ln -snfv ~/dots/"$f" ~/
 done
 
+ln -snfv ~/dots/dir/.i3/config ~/.i3/config
 ln -snfv ~/dots/dir/.config/fcitx ~/.config/fcitx
 ln -snfv ~/dots/dir/.config/fontconfig ~/.config/fontconfig
